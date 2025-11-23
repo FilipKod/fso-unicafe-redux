890cd6ef72601a2d0b6e669abb7685fa38a0aa0a
Filip Madunicky <yelidric@gmail.com>
2025-11-23 17:22:56 +0100

    Improves feedback app functionality and code style
    Refactors the feedback application to connect buttons to actions, display current state, and improve code style by adding linting rules.

--- src/reducers/counterReducer.js
+++ src/reducers/counterReducer.js
@@ -7,25 +7,25 @@ const initialState = {
 const counterReducer = (state = initialState, action) => {
   console.log(action)
   switch (action.type) {
-    case 'GOOD':
-      return {
-        ...state,
-        good: state.good + 1
-      }
-    case 'OK':
-      return {
-        ...state,
-        ok: state.ok + 1
-      }
-    case 'BAD':
-      return {
-        ...state,
-        bad: state.bad + 1
-      }
-    case 'RESET':
-      return initialState
-    default:
-      return state
+  case 'GOOD':
+    return {
+      ...state,
+      good: state.good + 1
+    }
+  case 'OK':
+    return {
+      ...state,
+      ok: state.ok + 1
+    }
+  case 'BAD':
+    return {
+      ...state,
+      bad: state.bad + 1
+    }
+  case 'RESET':
+    return initialState
+  default:
+    return state
   }
 }
 
